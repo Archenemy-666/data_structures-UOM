dbb759b720eae17678664c1108ef4e39fefbe087
Siddharth <csiddu98@outlook.com>
2020-07-14 18:46:29 +0530

    week1 complete

---
+++ week1-Strings.py
@@ -0,0 +1,19 @@
+print("hello")
+greet = 'Hello Bob'
+vamos = type(greet[0:4])
+print(vamos)
+str = 'X-DSPAM-Confidence:0.8475'
+print(type(str))
+loc = str.find(':')
+locend = len(str)
+l = type(str[loc+1:locend])
+v = str[loc+1:locend]
+type(v)
+m = float(v)
+print(type(m))
+print(m)
+# fl = print(str[loc+1:locend])
+# print(type(fl))
+# output type is NoneType
+
+
